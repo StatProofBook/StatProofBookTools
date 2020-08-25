a8b1bdd21026c4b69079ed93d338a061f7dbc6c7
Joram Soch <joram.soch@bccn-berlin.de>
2020-08-25 19:37:39 +0200

    added "visualize_all"
    The script "visualize_all.py" was added. It translates the entire table of contents into one nested dictionary that is later to be used for interactive visualization of the StatProofBook.

---
+++ visualize_all.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Visualize all items in the StatProofBook
+_
+This script prepares a Python dictionary coding the entire table of contents
+that is later to be used for interactive visualization of the StatProofBook.
+
+Author: Joram Soch, BCCN Berlin
+E-Mail: joram.soch@bccn-berlin.de
+
+First edit: 2020-08-25 19:25:00
+ Last edit: 2020-08-25 19:25:00
+"""
+
+
+# Import modules
+#-----------------------------------------------------------------------------#
+# import os
+import BookTools as spbt
+
+# Set repository directory
+#-----------------------------------------------------------------------------#
+rep_dir = spbt.get_rep_dir('offline')
+toc_md  = '/I/Table_of_Contents.md'
+
+# Load "Table of Contents"
+#-----------------------------------------------------------------------------#
+toc_obj = open(rep_dir + toc_md, 'r')
+toc_txt = toc_obj.readlines()
+toc_obj.close()
+
+# Browse through files
+#-----------------------------------------------------------------------------#
+StatProofBook     = dict()
+nums, tocs, files = spbt.get_all_items(toc_txt)
+for toc,file_md in zip(tocs,files):
+    file = file_md[0:-3]
+    if toc[0] not in StatProofBook:
+        StatProofBook[toc[0]] = dict()
+    if toc[1] not in StatProofBook[toc[0]]:
+        StatProofBook[toc[0]][toc[1]] = dict()
+    if toc[2] not in StatProofBook[toc[0]][toc[1]]:
+        StatProofBook[toc[0]][toc[1]][toc[2]] = dict()
+    if toc[3] not in StatProofBook[toc[0]][toc[1]][toc[2]]:
+        StatProofBook[toc[0]][toc[1]][toc[2]][toc[3]] = ''
+    StatProofBook[toc[0]][toc[1]][toc[2]][toc[3]] = 'https://statproofbook.github.io' + file
