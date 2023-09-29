4ffd54e11e1072d43d0fc1ed6582988a03ce6054
Joram Soch <joram.soch@bccn-berlin.de>
2023-09-29 22:22:11 +0200

    added "create_def"
    A new routine, "create_def.py" was added that takes a definition written as a LaTeX file and transfers it into a Markdown document (almost) ready for submission to the StatProofBook. This considerably speeds up content preparation - only need to add title, theorem, sources and definition ID at the end.

--- create_def.py
+++ create_def.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 """
-Transfer Proof from LaTeX File to Markdown Document
+Transfer Definition from LaTeX File to Markdown Document
 _
-This script takes a proof written in LaTeX and transfers it into a Markdown
-file (almost) ready for submission to the StatProofBook.
+This script takes a definition written in LaTeX and transfers it into a
+Markdown file (almost) ready for submission to the StatProofBook.
 
 Author: Joram Soch, BCCN Berlin
 E-Mail: joram.soch@bccn-berlin.de
 
-First edit: 2023-09-01 13:40:00
- Last edit: 2023-09-08 12:54:00
+First edit: 2023-09-22 10:55:00
+ Last edit: 2023-09-22 10:55:00
 """
 
 
-# Specify proofs
+# Specify definitions
 #-----------------------------------------------------------------------------#
-temp_proof = 'lfe-lme'          # Markdown proof to be used as template
-src_proof  = 'lfe-approx'       # LaTeX proof to be used as source
-src_dir    =r'C:\Users\sochj\ownCloud\StatProofBook\Submissions\JoramSoch\Proofs'
+temp_def = 'mvn'                # Markdown definition to be used as template
+src_def  = 'bvn'                # LaTeX definition to be used as source
+src_dir  =r'C:\Users\sochj\ownCloud\StatProofBook\Submissions\JoramSoch\Definitions'
 
 # Import modules
 #-----------------------------------------------------------------------------#
@@ -30,14 +30,14 @@ from datetime import datetime
 #-----------------------------------------------------------------------------#
 rep_dir = spbt.get_rep_dir('offline')
 
-# Load source proof
+# Load source definition
 #-----------------------------------------------------------------------------#
-file_tex = src_dir+'/'+src_proof+'.tex'
+file_tex = src_dir+'/'+src_def+'.tex'
 file_obj = open(file_tex, 'r')
 file_txt = file_obj.readlines()
 file_obj.close()
 
-# Analyze source proof
+# Analyze source definition
 #-----------------------------------------------------------------------------#
 begin = False
 i1 = 0; i2 = 0;
@@ -51,14 +51,14 @@ for (i, line) in enumerate(file_txt):
         begin = True
 body_txt = file_txt[i1:i2+1]
 
-# Load template proof
+# Load template definition
 #-----------------------------------------------------------------------------#
-file_md  = rep_dir+'/P/'+temp_proof+'.md'
+file_md  = rep_dir+'/D/'+temp_def+'.md'
 file_obj = open(file_md, 'r')
 file_txt = file_obj.readlines()
 file_obj.close()
 
-# Analyze template proof
+# Analyze template definition
 #-----------------------------------------------------------------------------#
 file_id, shortcut, title, username, date = spbt.get_meta_data(file_txt)
 chapter, section, topic, item            = spbt.get_toc_info(file_txt)
@@ -71,33 +71,33 @@ for (i, line) in enumerate(file_txt):
     if line.find('e_mail:') > -1:
         i3 = i
 
-# Write new proof header
+# Write new definition header
 #-----------------------------------------------------------------------------#
-proof_md = ['---\n',
-            'layout: proof\n',
-            'mathjax: true\n',
-            '\n',
-            file_txt[i1],
-            file_txt[i2],
-            file_txt[i3],
-            'date: '+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n',
-            '\n',
-            'title: "___"\n',
-            'chapter: "'+chapter+'"\n',
-            'section: "'+section+'"\n',
-            'topic: "'+topic+'"\n',
-            'theorem: "___"\n',
-            '\n',
-            'sources:\n',
-            '\n',
-            'proof_id: "___"\n',
-            'shortcut: "'+src_proof+'"\n',
-            'username: "'+username+'"\n',
-            '---\n',
-            '\n',
-            '\n']
+def_md = ['---\n',
+          'layout: definition\n',
+          'mathjax: true\n',
+          '\n',
+          file_txt[i1],
+          file_txt[i2],
+          file_txt[i3],
+          'date: '+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n',
+          '\n',
+          'title: "___"\n',
+          'chapter: "'+chapter+'"\n',
+          'section: "'+section+'"\n',
+          'topic: "'+topic+'"\n',
+          'definition: "___"\n',
+          '\n',
+          'sources:\n',
+          '\n',
+          'def_id: "___"\n',
+          'shortcut: "'+src_def+'"\n',
+          'username: "'+username+'"\n',
+          '---\n',
+          '\n',
+          '\n']
 
-# Write new proof body
+# Write new definition body
 #-----------------------------------------------------------------------------#
 in_equation = False
 for line in body_txt:
@@ -108,14 +108,14 @@ for line in body_txt:
     if in_equation and line.find('\\end{equation}') == 0:
         line = re.sub('\\\\end{equation}', '$$', line)
         in_equation = False
-    # append current line to proof body
-    proof_md.append(line)
+    # append current line to definition body
+    def_md.append(line)
 
-# Save and open new proof
+# Save and open new definition
 #-----------------------------------------------------------------------------#
-file_md  = src_dir+'/'+src_proof+'.md'
+file_md  = src_dir+'/'+src_def+'.md'
 file_obj = open(file_md, 'w')
-for line in proof_md:
+for line in def_md:
     file_obj.write(line)
 file_obj.close()
 webbrowser.open(file_md)
---
+++ create_proof.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+"""
+Transfer Proof from LaTeX File to Markdown Document
+_
+This script takes a proof written in LaTeX and transfers it into a Markdown
+file (almost) ready for submission to the StatProofBook.
+
+Author: Joram Soch, BCCN Berlin
+E-Mail: joram.soch@bccn-berlin.de
+
+First edit: 2023-09-01 13:40:00
+ Last edit: 2023-09-22 11:39:00
+"""
+
+
+# Specify proofs
+#-----------------------------------------------------------------------------#
+temp_proof = 'bvn-pdf'          # Markdown proof to be used as template
+src_proof  = 'bvn-pdfcorr'          # LaTeX proof to be used as source
+src_dir    =r'C:\Users\sochj\ownCloud\StatProofBook\Submissions\JoramSoch\Proofs'
+
+# Import modules
+#-----------------------------------------------------------------------------#
+import re
+import webbrowser
+import BookTools as spbt
+from datetime import datetime
+
+# Set repository directory
+#-----------------------------------------------------------------------------#
+rep_dir = spbt.get_rep_dir('offline')
+
+# Load source proof
+#-----------------------------------------------------------------------------#
+file_tex = src_dir+'/'+src_proof+'.tex'
+file_obj = open(file_tex, 'r')
+file_txt = file_obj.readlines()
+file_obj.close()
+
+# Analyze source proof
+#-----------------------------------------------------------------------------#
+begin = False
+i1 = 0; i2 = 0;
+for (i, line) in enumerate(file_txt):
+    if len(line) > 1:
+        if begin == True and i1 == 0:
+            i1 = i
+        if i1 > 0 and line.find('\\end{document}') < 0:
+            i2 = i
+    if line.find('\\begin{document}') > -1:
+        begin = True
+body_txt = file_txt[i1:i2+1]
+
+# Load template proof
+#-----------------------------------------------------------------------------#
+file_md  = rep_dir+'/P/'+temp_proof+'.md'
+file_obj = open(file_md, 'r')
+file_txt = file_obj.readlines()
+file_obj.close()
+
+# Analyze template proof
+#-----------------------------------------------------------------------------#
+file_id, shortcut, title, username, date = spbt.get_meta_data(file_txt)
+chapter, section, topic, item            = spbt.get_toc_info(file_txt)
+i1 = 0; i2 = 0; i3 = 0;
+for (i, line) in enumerate(file_txt):
+    if line.find('author:') > -1:
+        i1 = i
+    if line.find('affiliation:') > -1:
+        i2 = i
+    if line.find('e_mail:') > -1:
+        i3 = i
+
+# Write new proof header
+#-----------------------------------------------------------------------------#
+proof_md = ['---\n',
+            'layout: proof\n',
+            'mathjax: true\n',
+            '\n',
+            file_txt[i1],
+            file_txt[i2],
+            file_txt[i3],
+            'date: '+datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n',
+            '\n',
+            'title: "___"\n',
+            'chapter: "'+chapter+'"\n',
+            'section: "'+section+'"\n',
+            'topic: "'+topic+'"\n',
+            'theorem: "___"\n',
+            '\n',
+            'sources:\n',
+            '\n',
+            'proof_id: "___"\n',
+            'shortcut: "'+src_proof+'"\n',
+            'username: "'+username+'"\n',
+            '---\n',
+            '\n',
+            '\n']
+
+# Write new proof body
+#-----------------------------------------------------------------------------#
+in_equation = False
+for line in body_txt:
+    # correct equation enviroment (\begin/end{equation} -> $$...$$)
+    if not in_equation and line.find('\\begin{equation}') == 0:
+        line = re.sub('\\\\begin{equation}', '$$', line)
+        in_equation = True
+    if in_equation and line.find('\\end{equation}') == 0:
+        line = re.sub('\\\\end{equation}', '$$', line)
+        in_equation = False
+    # append current line to proof body
+    proof_md.append(line)
+
+# Save and open new proof
+#-----------------------------------------------------------------------------#
+file_md  = src_dir+'/'+src_proof+'.md'
+file_obj = open(file_md, 'w')
+for line in proof_md:
+    file_obj.write(line)
+file_obj.close()
+webbrowser.open(file_md)
